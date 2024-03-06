3a1293ec28ef37d215eec0d66d9bfb7ec64d4504
DmShko <vombat_schev@i.ua>
2024-03-06 12:26:40 +0200

    vombArt-105

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -13,11 +13,11 @@ const Footer = () => {
     <>
       <div className={fo.source}>{selectorGallSlice.dayNight ? <FireBaseLogoBlack style={{width: '130px'}}/> : <FireBaseLogoWhite style={{width: '130px'}}/>}</div>
       <p className={fo.sound}>Sound Effect by <a href="https://pixabay.com/ru/users/universfield-28281460/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=138807">UNIVERSFIELD</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=138807">Pixabay</a></p>
-      <div className={fo.container} style={selectorGallSlice.dayNight ? {backgroundColor: 'rgb(122, 152, 206)',} : {backgroundColor: '',}}>
-        {selectorGallSlice.settings.languageSelector === 'English' ? <p>&#169; Home photo by Viktor Hanacek</p> : 
-          selectorGallSlice.settings.languageSelector === 'Українська' ? <p>&#169; Фото головної сторінки - Viktor Hanacek</p> : 
-          selectorGallSlice.settings.languageSelector === 'Polska' ? <p>&#169; Zdjęcie domowe autorstwa Wiktora Hanacka</p> : <p>&#169; Home photo by Viktor Hanacek</p>}
-      </div>
+      <p className={fo.sound}>
+        {selectorGallSlice.settings.languageSelector === 'English' ? <span>Home photo by Viktor Hanacek</span> : 
+          selectorGallSlice.settings.languageSelector === 'Українська' ? <span>Фото головної сторінки - Viktor Hanacek</span> : 
+          selectorGallSlice.settings.languageSelector === 'Polska' ? <span>Zdjęcie domowe autorstwa Wiktora Hanacka</span> : <span>Home photo by Viktor Hanacek</span>}
+      </p>
       <div className={fo.container} style={selectorGallSlice.dayNight ? {backgroundColor: 'rgb(122, 152, 206)',} : {backgroundColor: '',}}>
         {selectorGallSlice.settings.languageSelector === 'English' ? <p>&#169; Developed by Dmytro Shevchenko</p> : 
           selectorGallSlice.settings.languageSelector === 'Українська' ? <p>&#169; Розроблено Дмитром Шевченко</p> : 
