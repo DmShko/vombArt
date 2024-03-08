4b1238847d5a618309d3f2465aa02d68bbf4213a
DmShko <vombat_schev@i.ua>
2024-03-08 17:41:37 +0200

    vombArt-115

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -452,10 +452,9 @@ const Account = () => {
             };
           
           };
-          return total / selectorGallerySlice.itemsBuffer.length;
       };
 
-      
+      return total / selectorGallerySlice.itemsBuffer.length;
     };
 
     const changeBorderOver = (evt) => {
